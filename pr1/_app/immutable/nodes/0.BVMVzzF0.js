19bc6cfaa561436508b8aec74b55d6e1303525b3
anicolao <anicolao@users.noreply.github.com>
2026-07-30 11:35:25 +0000

    deploy: 58ce27569335c7e2ad258a90d3f69a197e070fbc

---
+++ pr1/_app/immutable/nodes/0.BVMVzzF0.js
@@ -0,0 +1,1 @@
+import{L as e,X as t,b as n,h as r,x as i}from"../chunks/D2OJTCAO.js";import"../chunks/xihTtKlq.js";var a=t({prerender:()=>!0,ssr:()=>!1});function o(t,a){var o=i();r(e(o),()=>a.children),n(t,o)}export{o as component,a as universal};
